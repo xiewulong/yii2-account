f75b51b592fdbdb900b3d2033fe799d87f3be95f
xiewulong <xiewulong@vip.qq.com>
2016-08-07 11:43:50 +0800

    account

--- Module.php
+++ Module.php
@@ -1,12 +1,12 @@
 <?php
 /*!
- * yii2 extension - account - module
+ * yii2 - module - account
  * xiewulong <xiewulong@vip.qq.com>
  * https://github.com/xiewulong/yii2-account
  * https://raw.githubusercontent.com/xiewulong/yii2-account/master/LICENSE
  * create: 2016/7/27
- * update: 2016/8/1
- * version: 0.0.1
+ * update: 2016/8/7
+ * since: 0.0.1
  */
 
 namespace yii\account;
--- migrations/m160731_032036_account_init.php
+++ migrations/m160731_032036_account_init.php
@@ -1,5 +1,4 @@
 <?php
-
 use yii\db\Migration;
 
 class m160731_032036_account_init extends Migration {
