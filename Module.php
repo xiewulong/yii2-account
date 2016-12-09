586066d62f846c1073d43e28e9dcf61ff6c22a7d
xiewulong <xiewulong@vip.qq.com>
2016-12-09 17:41:38 +0800

    account

--- Module.php
+++ Module.php
@@ -5,7 +5,7 @@
  * https://github.com/xiewulong/yii2-account
  * https://raw.githubusercontent.com/xiewulong/yii2-account/master/LICENSE
  * create: 2016/7/27
- * update: 2016/9/6
+ * update: 2016/12/9
  * since: 0.0.1
  */
 
@@ -15,20 +15,20 @@ use Yii;
 
 class Module extends \yii\components\Module {
 
+	public $defaultRoute = 'user';
+
+	public $messageCategory = 'account';
+
+	public $permissions = ['@'];
+
 	public $identityClass = 'yii\account\models\User';
 
 	public $layout = 'account';
-
-	public $defaultRoute = 'user';
-
-	public $permissions = ['@'];
 
 	public $author = 'xiewulong<xiewulong@vip.qq.com>';
 
 	public $keywords = 'yii2, xiewulong, account, user, login, signin, signup, password, reset';
 
 	public $description = 'yii2 account management module';
-
-	public $messageCategory = 'account';
 
 }
